47a0d7760e1539a9af85f7fa0f31bd328150e699
raznara <raznara@s09-prof>
2017-10-20 14:01:49 +0200

    license

--- src/main/java/eu/rafaelaznar/connection/ConnectionInterface.java
+++ src/main/java/eu/rafaelaznar/connection/ConnectionInterface.java
@@ -6,7 +6,7 @@
  *
  * Sources at https://github.com/rafaelaznar/carrito-server
  * 
- * sissane-server is distributed under the MIT License (MIT)
+ * carrito-server is distributed under the MIT License (MIT)
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
