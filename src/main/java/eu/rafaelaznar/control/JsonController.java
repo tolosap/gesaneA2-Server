89e2932a425c94c9c29a882bfdc5a12b059c4b06
rafa aznar <rafaaznar@gmail.com>
2017-11-12 15:51:29 +0100

    favicon

--- src/main/java/eu/rafaelaznar/control/JsonController.java
+++ src/main/java/eu/rafaelaznar/control/JsonController.java
@@ -78,7 +78,7 @@ public class JsonController extends HttpServlet {
                 response.setContentType("text/html;charset=UTF-8");
                 out.println("<!DOCTYPE html>");
                 out.println("<html>");
-                out.println("<head><title>Trolleyes server</title></head>");
+                out.println("<head><title>Trolleyes server</title><link rel=\"shortcut icon\" href=\"favicon.ico\" type=\"image/x-icon\"></head>");
                 out.println("<body style=\"background: url(trolleyes400.png) no-repeat center center fixed;\">");
                 out.println("<h1>Welcome to trolleyes server</h1><h2>Servlet controller json listening at " + InetAddress.getLocalHost().getHostAddress() + ":" + request.getLocalPort() + request.getContextPath() + "</h2>");
                 out.println("");
