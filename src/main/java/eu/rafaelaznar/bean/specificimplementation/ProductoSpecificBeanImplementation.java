2913b04e3b3ef76a06f04440b28f428be23c3d2b
rafa aznar <rafaaznar@gmail.com>
2017-11-18 07:06:05 +0100

    precio type

--- src/main/java/eu/rafaelaznar/bean/specificimplementation/ProductoSpecificBeanImplementation.java
+++ src/main/java/eu/rafaelaznar/bean/specificimplementation/ProductoSpecificBeanImplementation.java
@@ -131,7 +131,7 @@ public class ProductoSpecificBeanImplementation extends TableGenericBeanImplemen
         this.setCodigo(oResultSet.getString("codigo"));
         this.setDescripcion(oResultSet.getString("descripcion"));
         this.setExistencias(oResultSet.getInt("existencias"));
-        this.setPrecio(oResultSet.getInt("precio"));
+        this.setPrecio(oResultSet.getDouble("precio"));
         return this;
     }
 
