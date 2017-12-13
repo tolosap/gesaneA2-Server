d934816166ae975e6f44afeac3486d9480836d84
a029210979j <a029210979j@S09-PC16>
2017-12-13 10:06:45 +0100

    gesane-destinoalta-completo

--- src/main/java/eu/rafaelaznar/service/specificimplementation/DestinoaltaSpecificServiceImplementation.java
+++ src/main/java/eu/rafaelaznar/service/specificimplementation/DestinoaltaSpecificServiceImplementation.java
@@ -6,22 +6,50 @@
 package eu.rafaelaznar.service.specificimplementation;
 
 import eu.rafaelaznar.bean.helper.MetaBeanHelper;
+import eu.rafaelaznar.bean.specificimplementation.UsuarioSpecificBeanImplementation;
 import eu.rafaelaznar.service.genericimplementation.TableGenericServiceImplementation;
 import javax.servlet.http.HttpServletRequest;
 
-public class DestinoaltaSpecificServiceImplementation extends TableGenericServiceImplementation{
-    
+public class DestinoaltaSpecificServiceImplementation extends TableGenericServiceImplementation {
+
     public DestinoaltaSpecificServiceImplementation(HttpServletRequest request) {
         super(request);
     }
- 
-@Override    
-protected Boolean checkPermission(String strMethodName) {
+
+    @Override
+    protected Boolean checkPermission(String strMethodName) {
+        strMethodName.toLowerCase();
         MetaBeanHelper oUsuarioBean = (MetaBeanHelper) oRequest.getSession().getAttribute("user");
-        if (oUsuarioBean != null) {
+        UsuarioSpecificBeanImplementation oUser = (UsuarioSpecificBeanImplementation) oUsuarioBean.getBean();
+        if (oUsuarioBean != null && oUser.getId_tipousuario() == 1) {
             return true;
+        } else if (oUsuarioBean != null && oUser.getId_tipousuario() >= 3 && oUser.getId_tipousuario() <= 5) {
+            switch (strMethodName) {
+                case "getmetadata":
+                    return true;
+                case "getobjectmetadata":
+                    return true;
+                case "getpropertiesmetadata":
+                    return true;
+                case "get":
+                    return true;
+                case "set":
+                    return false;
+                case "remove":
+                    return false;
+                case "getpage":
+                    return true;
+                case "getcount":
+                    return true;
+                case "getpagex":
+                    return true;
+                case "getcountx":
+                    return true;
+                default:
+                    return false;
+            }
         } else {
             return false;
         }
-    }    
+    }
 }
