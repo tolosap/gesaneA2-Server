6179e100a10a87a80d36d2732c97042380057956
raznara <raznara@s09-prof>
2017-12-14 12:43:25 +0100

    ultramarronazo 2.0 checkpermission fixed

--- src/main/java/eu/rafaelaznar/service/specificimplementation/EspecialidadSpecificServiceImplementation.java
+++ src/main/java/eu/rafaelaznar/service/specificimplementation/EspecialidadSpecificServiceImplementation.java
@@ -1,23 +1,23 @@
 /*
  * Copyright (c) 2017 by Rafael Angel Aznar Aparici (rafaaznar at gmail dot com)
- * 
- * trolleyes-server3: Helps you to develop easily AJAX web applications 
+ *
+ * trolleyes-server3: Helps you to develop easily AJAX web applications
  *               by copying and modifying this Java Server.
  *
  * Sources at https://github.com/rafaelaznar/trolleyes-server3
- * 
+ *
  * trolleyes-server3 is distributed under the MIT License (MIT)
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,6 +29,7 @@
 package eu.rafaelaznar.service.specificimplementation;
 
 import eu.rafaelaznar.bean.helper.MetaBeanHelper;
+import eu.rafaelaznar.bean.specificimplementation.TipousuarioSpecificBeanImplementation;
 import eu.rafaelaznar.bean.specificimplementation.UsuarioSpecificBeanImplementation;
 import eu.rafaelaznar.service.genericimplementation.TableGenericServiceImplementation;
 import javax.servlet.http.HttpServletRequest;
@@ -62,30 +63,41 @@ public class EspecialidadSpecificServiceImplementation extends TableGenericServi
     @Override
     protected Boolean checkPermission(String strMethodName) {
         MetaBeanHelper oUsuarioBean = (MetaBeanHelper) oRequest.getSession().getAttribute("user");
-        UsuarioSpecificBeanImplementation oUsuario = (UsuarioSpecificBeanImplementation) oUsuarioBean.getBean();
-        String strMethod = strMethodName.toLowerCase();
-        if (oUsuarioBean != null && oUsuario.getId_tipousuario() == 1) {
-            return true;
-        } else if (oUsuario.getId_tipousuario() == 3
-                || oUsuario.getId_tipousuario() == 4
-                || oUsuario.getId_tipousuario() == 5) {
-            switch (strMethod) {
-                case "getmetadata":
-                    return true;
-                case "getobjectmetadata":
-                    return true;
-                case "getpropertiesmetadata":
-                    return true;
-                case "get":
-                    return true;
-                case "set":
+        if (oUsuarioBean != null) {
+            UsuarioSpecificBeanImplementation oUsuario = (UsuarioSpecificBeanImplementation) oUsuarioBean.getBean();
+            MetaBeanHelper oMetaBeanHelper = oUsuario.getObj_tipousuario();
+            TipousuarioSpecificBeanImplementation oTipousuario = (TipousuarioSpecificBeanImplementation) oMetaBeanHelper.getBean();
+            Integer idTipousuario = oTipousuario.getId();
+
+            String strMethod = strMethodName.toLowerCase();
+            if (idTipousuario == 1) {
+                return true;
+            } else {
+                if (idTipousuario == 3
+                        || idTipousuario == 4
+                        || idTipousuario == 5) {
+
+                    switch (strMethod) {
+                        case "getmetadata":
+                            return true;
+                        case "getobjectmetadata":
+                            return true;
+                        case "getpropertiesmetadata":
+                            return true;
+                        case "get":
+                            return true;
+                        case "set":
+                            return false;
+                        case "remove":
+                            return false;
+                        case "getpage":
+                            return true;
+                        case "getcount":
+                            return true;
+                    }
+                } else {
                     return false;
-                case "remove":
-                    return false;
-                case "getpage":
-                    return true;
-                case "getcount":
-                    return true;
+                }
             }
         } else {
             return false;
