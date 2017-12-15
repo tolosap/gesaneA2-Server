3bbe2578b609402d798791ef32e28365bc9e8d9c
a029210979j <a029210979j@S09-PC16>
2017-12-15 10:14:03 +0100

    gesane-server-tiposervicio-done

--- src/main/java/eu/rafaelaznar/service/specificimplementation/UsuarioSpecificServiceImplementation.java
+++ src/main/java/eu/rafaelaznar/service/specificimplementation/UsuarioSpecificServiceImplementation.java
@@ -86,6 +86,8 @@ public class UsuarioSpecificServiceImplementation extends TableGenericServiceImp
         hmObjectsMetaData.put("sexo", oDao.getObjectMetaData());
         oDao = DaoFactory.getDao("circunstanciasalta", null, (MetaBeanHelper) oRequest.getSession().getAttribute("user"), null);
         hmObjectsMetaData.put("circunstanciasalta", oDao.getObjectMetaData());
+        oDao = DaoFactory.getDao("tiposervicio", null, (MetaBeanHelper) oRequest.getSession().getAttribute("user"), null);
+        hmObjectsMetaData.put("tiposervicio", oDao.getObjectMetaData());
 
         String strJson = GsonHelper.getGson().toJson(hmObjectsMetaData);
         oReplyBean = new ReplyBeanHelper(200, strJson);
