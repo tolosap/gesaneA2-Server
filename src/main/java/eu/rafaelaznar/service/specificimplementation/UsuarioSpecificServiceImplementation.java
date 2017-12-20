d5a506091a0bd64f3fec28d20574b2d6719a3b59
a053629318b <a053629318b@S09-PC08>
2017-12-20 09:39:21 +0100

    tabla administrador medico Nico y Bruno

--- src/main/java/eu/rafaelaznar/service/specificimplementation/UsuarioSpecificServiceImplementation.java
+++ src/main/java/eu/rafaelaznar/service/specificimplementation/UsuarioSpecificServiceImplementation.java
@@ -107,7 +107,8 @@ public class UsuarioSpecificServiceImplementation extends TableGenericServiceImp
         hmObjectsMetaData.put("categoriaprofesional", oDao.getObjectMetaData());
         oDao = DaoFactory.getDao("episodio", null, (MetaBeanHelper) oRequest.getSession().getAttribute("user"), null);
         hmObjectsMetaData.put("episodio", oDao.getObjectMetaData());
-
+        oDao = DaoFactory.getDao("medico", null, (MetaBeanHelper) oRequest.getSession().getAttribute("user"), null);
+        hmObjectsMetaData.put("medico", oDao.getObjectMetaData());
         String strJson = GsonHelper.getGson().toJson(hmObjectsMetaData);
         oReplyBean = new ReplyBeanHelper(200, strJson);
         return oReplyBean;
