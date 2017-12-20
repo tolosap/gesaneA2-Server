31dd306b68cf934ae2e7ca147a26a1e3f1dda641
a029210979j <a029210979j@S09-PC16>
2017-12-20 08:37:16 +0100

    gesane-cliente-DaoPaciente-WiP-

--- src/main/java/eu/rafaelaznar/bean/specificimplementation/PacienteSpecificBeanImplementation.java
+++ src/main/java/eu/rafaelaznar/bean/specificimplementation/PacienteSpecificBeanImplementation.java
@@ -37,14 +37,8 @@ import eu.rafaelaznar.bean.genericimplementation.TableGenericBeanImplementation;
 import eu.rafaelaznar.bean.helper.MetaBeanHelper;
 import eu.rafaelaznar.bean.meta.publicinterface.MetaObjectBeanInterface;
 import eu.rafaelaznar.bean.meta.publicinterface.MetaPropertyBeanInterface;
-import eu.rafaelaznar.dao.specificimplementation.GrupoSpecificDaoImplementation;
 import eu.rafaelaznar.helper.EnumHelper;
-import eu.rafaelaznar.helper.Log4jHelper;
 import eu.rafaelaznar.helper.constant.RegexConstants;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.Date;
 
 @MetaObjectBeanInterface(
@@ -331,11 +325,9 @@ public class PacienteSpecificBeanImplementation extends TableGenericBeanImplemen
             Description = "Sip del paciente",
             Type = EnumHelper.FieldType.String,
             IsRequired = true,
-            RegexPattern = RegexConstants.capitalizedName,
-            RegexHelp = RegexConstants.capitalizedName_Help,
             IsForeignKeyDescriptor = false,
             Wide = 3,
-            MaxLength = 100,
+            MaxLength = 20,
             IsVisible = false
     )
     private String sip_aseguradora;
